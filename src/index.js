d096c7a39ac09a2c05837a97b3889e78c6fa881a
Warren Parad <gpg@warrenparad.net>
2023-08-30 21:32:44 +0200

    Use pull request and workflow list to trigger rerun.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,50 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const stringify = require('json-stringify-safe');
+const { DateTime } = require('luxon');
 
-async function run() {
+async function runAction() {
   // Attempt to load credentials from the GitHub OIDC provider.
   const githubSecretAccessToken = core.getInput('github_token');
   if (!githubSecretAccessToken || githubSecretAccessToken === '{{ secrets.GITHUB_TOKEN }}') {
+    // GitHub core library has a critical failure if we don't escape the $, seems like a malicious attack vector they aren't handling correctly
+    // eslint-disable-next-line no-useless-escape
     core.setFailed("Missing use with configuration in the github action, please add to the github workflow: 'github_token: ${{ secrets.GITHUB_TOKEN }}'");
     core.getInput('github_token', { required: true });
     throw Error('InvalidInput');
   }
 
   // https://docs.github.com/en/actions/learn-github-actions/contexts#example-contents-of-the-github-contex
-  const { repository, repository_owner: owner, ref_name: currentRef, ref_type: triggerType } = github.context;
-  if (!triggerType) {
-    core.info(stringify(github));
-    core.setFailed('No trigger type set');
-    throw Error('InvalidInput');
-  }
-  if (triggerType !== 'branch') {
-    core.info(`Skipping check because trigger type is not branch. Trigger Type: ${triggerType}, Ref: ${currentRef}`);
-    return;
-  }
+  const currentRef = github.context.payload.ref.replace(/^refs\/heads\//, '');
+  const workflowTarget = github.context.workflow;
+  core.info(`Branch Ref: ${currentRef}, Workflow: ${workflowTarget}`);
+  const [owner, repo] = github.context.payload.repository.full_name.split('/');
 
   const octokit = github.getOctokit(githubSecretAccessToken);
-  const branches = await octokit.rest.repos.listBranches({
-    owner: owner,
-    repo: repository
+
+  const openPullRequestsResponse = await octokit.rest.pulls.list({ owner, repo, state: 'open', base: currentRef, per_page: 100 });
+  core.info(`Relevant open pull requests: [${openPullRequestsResponse.data.map(pr => pr.number).join(', ')}]`);
+
+  const prMap = openPullRequestsResponse.data.reduce((acc, pr) => { acc[pr.number] = true; return acc; }, {});
+
+  const workflowRuns = await octokit.rest.actions.listWorkflowRuns({
+    owner, repo, workflow_id: workflowTarget, event: 'pull_request', created: `>= ${DateTime.utc().minus({ month: 1 }).toISODate()}`, per_page: 100
   });
-  // https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
-  const filteredBranches = branches.data.filter(branch => branch.name !== currentRef);
-  await Promise.all(filteredBranches.map(async branch => {
+
+  const workflowRunsForAffectedPrs = workflowRuns.data.workflow_runs.filter(run => run.pull_requests?.some(pr => prMap[pr.number]));
+  await Promise.all(workflowRunsForAffectedPrs.map(async run => {
     try {
-      await octokit.rest.repos.createDispatchEvent({
-        owner: owner,
-        repo: repository,
-        workflow_id: github.workflow,
-        ref: branch
+      await core.info(`Attempting to rerun: ${run.id}`);
+      await octokit.rest.actions.reRunWorkflow({
+        owner, repo, run_id: run.run_id
       });
     } catch (error) {
-      core.error(`Failed to automatically trigger branch ${branch}: ${error.code} - ${error.message}`);
+      core.error(`Failed to automatically retrigger pull request: ${run.pull_requests?.map(pr => pr.number).join(',')}: ${error.code} - ${error.message}`);
     }
   }));
 }
 
-exports.run = run;
+exports.run = runAction;
 
 if (require.main === module) {
-  run();
+  runAction();
 }
